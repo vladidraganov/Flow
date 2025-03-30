05a4184013b85aee1fe9b170c278ea37be3f0123
Vladislav Draganov <vladidraganov89@gmail.com>
2025-03-30 23:40:01 +0300

    new go back button for login and sign up page

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import Button from "@/components/button";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "expo-router";
@@ -27,6 +27,18 @@ const LoginScreen = () => {
 
   return (
     <View className="flex-1 bg-[#0A0E1F] px-6 pt-20 justify-start ">
+      <TouchableOpacity
+      className="absolute top-5 mt-8 left-6 p-3 border w-full max-w-[40px] h-full max-h-[40px] items-center border-[#5C5E67] rounded-2xl"
+      onPress={() => {
+        router.push("/signup");
+      }}
+      >
+      <Image
+        source={require("@/assets/images/back-arrow.png")}
+        className="w-4 h-4 "
+        style={{ tintColor: "#5C5E67" }}
+      />
+      </TouchableOpacity>
       <Text className="text-white text-3xl font-semibold ml-6 mt-16 mb-4">Login</Text>
 
       <View className="mt-6 w-full  items-center">
